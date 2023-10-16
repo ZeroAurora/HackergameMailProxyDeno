30962c5e1f4c75daee3a8f2560d93f433206ab08
ZeroAurora <zeroaurora233@outlook.com>
2023-10-16 22:21:52 +0800

    fuck deno redis

--- src/redis.ts
+++ src/redis.ts
@@ -1,33 +1,30 @@
-import { connect, RedisConnectOptions } from "redis";
+import { createClient } from "redis";
 import { encodeHex } from "std/encoding/hex";
 import { IHGMailRequest, SendMailStatus } from "./types.ts";
 import { env } from "./env.ts";
 
 // redis config
-const redisConfig: RedisConnectOptions = {
-  hostname: env.REDIS_HOST,
-  port: Number(env.REDIS_PORT),
-};
+const client = createClient({url: `redis://${env.REDIS_HOST}:${env.REDIS_PORT}`});
 
 // traffic control using redis
 export async function storeTraffic(mail: IHGMailRequest) {
-  const redis = await connect(redisConfig);
+  const redis = await client.connect();
 
   const contentHash = encodeHex(
     await crypto.subtle.digest("SHA-1", new TextEncoder().encode(mail.body)),
   );
-  await redis.setex(`mails:by-ip:${mail.ip}:${contentHash}`, 60 * 60, "1");
-  await redis.setex(`mails:by-email:${mail.to}:${contentHash}`, 60 * 60, "1");
+  await redis.setEx(`mails:by-ip:${mail.ip}:${contentHash}`, 60 * 60, "1");
+  await redis.setEx(`mails:by-email:${mail.to}:${contentHash}`, 60 * 60, "1");
 
-  redis.close();
+  await redis.disconnect();
   console.log(`[REDIS|TRAFFIC] Stored traffic for ${mail.to} (${mail.ip})`);
 }
 
 export async function checkTraffic(mail: IHGMailRequest) {
-  const redis = await connect(redisConfig);
+  const redis = await client.connect();
   const countByIP = (await redis.keys(`mails:by-ip:${mail.ip}:*`)).length;
   const countByEmail = (await redis.keys(`mails:by-email:${mail.to}:*`)).length;
-  redis.close();
+  redis.disconnect();
 
   if (countByIP > Number(env.IP_LIMIT)) {
     console.log(`[REDIS|TRAFFIC] IP limit exceeded for ${mail.ip}`);
